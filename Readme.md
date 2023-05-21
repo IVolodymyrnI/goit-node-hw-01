5cadf3e990362ae3644dd74c96c3ac7bb262233e
IVolodymyrnI <volodia.iarcguk.forwork@gmail.com>
2023-05-21 09:14:38 +0300

    ADDED: crud method

---
+++ Readme.md
@@ -0,0 +1,18 @@
+# goit-node-hw-01
+
+### GET list of contacts
+
+[screenshot's link](https://monosnap.com/file/pSllJmL0H9WKjzEYrZX7XIm9ZHoBOq)
+
+### POST
+
+[screenshot's link](https://monosnap.com/file/mVM3qdtHz9G60XT7On8YYLm8jhwTS0)
+
+### GET by id
+
+[screenshot's link](https://monosnap.com/file/oVOQL7z7iSy9gdfRXjc3xKmwwphPZo)
+
+
+### DELETE by id
+
+[screenshot's link](https://monosnap.com/file/f5Qokc6KtRZgFPhYtKFOpN3QOu7ZBa)
---
+++ index.js
@@ -0,0 +1,40 @@
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+} = require("./db/contacts");
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contacts = await listContacts();
+      return console.log(contacts);
+    case "get":
+      const contactById = await getContactById(id);
+      return console.log(contactById);
+    case "add":
+      const addedContact = await addContact({ name, email, phone });
+      return console.log(addedContact);
+    case "remove":
+      const deletedContact = await removeContact(id);
+      return console.log(deletedContact);
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
